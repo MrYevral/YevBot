9195a3bdd0418ca2e303ff3997d3ce3c973b578d
MrYevral <hac22@aber.ac.uk>
2014-06-13 16:53:38 +0100

    Playing around with MasterBot

--- Alternate/MasterBot.py
+++ Alternate/MasterBot.py
@@ -2,4 +2,15 @@
 will look for a .bot file, if that isn't found it will promt the user for auth
 tokens :- with this information the masterbot will connect to its own twitch channel and await a !connect command'''
 #Author MrYevral
-
+#check for .bot file in current directory
+import os
+import sys
+def getBotInfo():
+	if len(sys.argv) > 2:
+		if sys.argv[2] == '-c':
+			newBotFile()
+		else:
+			print "incorrect use of flags please use -c for creating a bot"
+'''for file in os.listdir("."):
+	if file.endswith(".bot"):
+		print file'''
