f64884472f60274ed46990321e7a1d60bec0a46f
MrYevral <hac22@aber.ac.uk>
2014-06-13 16:31:46 +0100

    set up alternate approach files

---
+++ Alternate/ChildBot.py
@@ -0,0 +1,2 @@
+'''This bot will be the template spawned by the master bot upon a successful 
+!connect command '''
---
+++ Alternate/MasterBot.py
@@ -0,0 +1,5 @@
+'''This gile will take arguments from the command line, if none are found it
+will look for a .bot file, if that isn't found it will promt the user for auth
+tokens :- with this information the masterbot will connect to its own twitch channel and await a !connect command'''
+#Author MrYevral
+
